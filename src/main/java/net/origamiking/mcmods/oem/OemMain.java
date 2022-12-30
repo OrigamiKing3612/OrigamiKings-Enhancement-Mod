edfc5690d37def382745fe22dd465868fd44ebd4
malachihernandez <glaspell.malachi@gmail.com>
2022-12-30 11:21:45 -0600

    updated textures and cleaned up code

--- src/main/java/net/origamiking/mcmods/oem/OemMain.java
+++ src/main/java/net/origamiking/mcmods/oem/OemMain.java
@@ -6,8 +6,6 @@ import net.origamiking.mcmods.oem.groups.ModGroups;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 public class OemMain implements ModInitializer {
 	public static final String MOD_ID = "oem";
 	public static final Logger LOGGER = LoggerFactory.getLogger("oem");
