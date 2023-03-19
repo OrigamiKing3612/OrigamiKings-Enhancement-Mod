081bc945d3e9b140e215adb52b482d2fe388f71d
OrigamiKing3612 <origamiking3612@gmail.com>
2023-03-19 13:28:31 -0500

    Cleaned up some code

--- src/main/java/net/origamiking/mcmods/oem/extra/commands/ModExtraCommands.java
+++ src/main/java/net/origamiking/mcmods/oem/extra/commands/ModExtraCommands.java
@@ -5,7 +5,7 @@ import net.origamiking.mcmods.oem.OemMain;
 
 public class ModExtraCommands {
     public static void register() {
-        OemMain.LOGGER.info("Adding Extra Commands to " + OemMain.NAME);
+        OemMain.LOGGER.info("Registering Extra Commands to " + OemMain.NAME);
         CommandsUtil.simpleStringReturnCommand("Server-Rules", "1. No stealing\n2. No griefing\n3. Play nice\n4. No inappropriate stuff (ever)\n5. No chat reporting\n6. Have fun");
         CommandsUtil.makeVersionCommand("oem-extra", "1.0.0-1.19.4");
     }
