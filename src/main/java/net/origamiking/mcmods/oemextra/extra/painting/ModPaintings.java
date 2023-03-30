466faf83be773aa324515dfeadf1d5bc2532e4c5
OrigamiKing3612 <origamiking3612@gmail.com>
2023-03-30 12:27:24 -0500

    Port to 1.18.2

--- src/main/java/net/origamiking/mcmods/oemextra/extra/painting/ModPaintings.java
+++ src/main/java/net/origamiking/mcmods/oemextra/extra/painting/ModPaintings.java
@@ -1,15 +1,15 @@
 package net.origamiking.mcmods.oemextra.extra.painting;
 
-import net.minecraft.entity.decoration.painting.PaintingVariant;
+import net.minecraft.entity.decoration.painting.PaintingMotive;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.origamiking.mcmods.oem.OemMain;
 
 public class ModPaintings {
-    public static final PaintingVariant SUS = registerPainting("sus", new PaintingVariant(32, 32));
+    public static final PaintingMotive SUS = registerPainting("sus", new PaintingMotive(32, 32));
 
-    private static PaintingVariant registerPainting(String name, PaintingVariant paintingVariant) {
-        return Registry.register(Registry.PAINTING_VARIANT, new Identifier(OemMain.MOD_ID, name), paintingVariant);
+    private static PaintingMotive registerPainting(String name, PaintingMotive paintingVariant) {
+        return Registry.register(Registry.PAINTING_MOTIVE, new Identifier(OemMain.MOD_ID, name), paintingVariant);
     }
     public static void register() {
         OemMain.LOGGER.info("Registering Extra Paintings for " + OemMain.NAME);
