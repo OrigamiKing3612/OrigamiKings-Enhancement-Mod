8add1ea5cb97101d169d76244a27680a2be8a786
OrigamiKing3612 <origamiking3612@gmail.com>
2023-05-13 15:47:44 -0500

    Added new recipes

---
+++ src/main/java/net/origamiking/mcmods/oem/screen/woodcutter/WoodcutterScreen.java
@@ -0,0 +1,175 @@
+package net.origamiking.mcmods.oem.screen.woodcutter;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.origamiking.mcmods.oem.recipe.WoodcutterRecipe;
+
+import java.util.List;
+
+@Environment(value= EnvType.CLIENT)
+public class WoodcutterScreen extends HandledScreen<WoodcutterScreenHandler> {
+    private static final Identifier TEXTURE = new Identifier("textures/gui/container/stonecutter.png");
+    private static final int SCROLLBAR_WIDTH = 12;
+    private static final int SCROLLBAR_HEIGHT = 15;
+    private static final int RECIPE_LIST_COLUMNS = 4;
+    private static final int RECIPE_LIST_ROWS = 3;
+    private static final int RECIPE_ENTRY_WIDTH = 16;
+    private static final int RECIPE_ENTRY_HEIGHT = 18;
+    private static final int SCROLLBAR_AREA_HEIGHT = 54;
+    private static final int RECIPE_LIST_OFFSET_X = 52;
+    private static final int RECIPE_LIST_OFFSET_Y = 14;
+    private float scrollAmount;
+    private boolean mouseClicked;
+    private int scrollOffset;
+    private boolean canCraft;
+
+    public WoodcutterScreen(WoodcutterScreenHandler handler, PlayerInventory inventory, Text title) {
+        super(handler, inventory, title);
+        handler.setContentsChangedListener(this::onInventoryChange);
+        --this.titleY;
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.render(matrices, mouseX, mouseY, delta);
+        this.drawMouseoverTooltip(matrices, mouseX, mouseY);
+    }
+
+    @Override
+    protected void drawBackground(MatrixStack matrices, float delta, int mouseX, int mouseY) {
+        this.renderBackground(matrices);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        int i = this.x;
+        int j = this.y;
+        WoodcutterScreen.drawTexture(matrices, i, j, 0, 0, this.backgroundWidth, this.backgroundHeight);
+        int k = (int)(41.0f * this.scrollAmount);
+        WoodcutterScreen.drawTexture(matrices, i + 119, j + 15 + k, 176 + (this.shouldScroll() ? 0 : 12), 0, 12, 15);
+        int l = this.x + 52;
+        int m = this.y + 14;
+        int n = this.scrollOffset + 12;
+        this.renderRecipeBackground(matrices, mouseX, mouseY, l, m, n);
+        this.renderRecipeIcons(matrices, l, m, n);
+    }
+
+    @Override
+    protected void drawMouseoverTooltip(MatrixStack matrices, int x, int y) {
+        super.drawMouseoverTooltip(matrices, x, y);
+        if (this.canCraft) {
+            int i = this.x + 52;
+            int j = this.y + 14;
+            int k = this.scrollOffset + 12;
+            List<WoodcutterRecipe> list = ((WoodcutterScreenHandler)this.handler).getAvailableRecipes();
+            for (int l = this.scrollOffset; l < k && l < ((WoodcutterScreenHandler)this.handler).getAvailableRecipeCount(); ++l) {
+                int m = l - this.scrollOffset;
+                int n = i + m % 4 * 16;
+                int o = j + m / 4 * 18 + 2;
+                if (x < n || x >= n + 16 || y < o || y >= o + 18) continue;
+                this.renderTooltip(matrices, list.get(l).getOutput(this.client.world.getRegistryManager()), x, y);
+            }
+        }
+    }
+
+    private void renderRecipeBackground(MatrixStack matrices, int mouseX, int mouseY, int x, int y, int scrollOffset) {
+        for (int i = this.scrollOffset; i < scrollOffset && i < ((WoodcutterScreenHandler)this.handler).getAvailableRecipeCount(); ++i) {
+            int j = i - this.scrollOffset;
+            int k = x + j % 4 * 16;
+            int l = j / 4;
+            int m = y + l * 18 + 2;
+            int n = this.backgroundHeight;
+            if (i == ((WoodcutterScreenHandler)this.handler).getSelectedRecipe()) {
+                n += 18;
+            } else if (mouseX >= k && mouseY >= m && mouseX < k + 16 && mouseY < m + 18) {
+                n += 36;
+            }
+            WoodcutterScreen.drawTexture(matrices, k, m - 1, 0, n, 16, 18);
+        }
+    }
+
+    private void renderRecipeIcons(MatrixStack matrices, int x, int y, int scrollOffset) {
+        List<WoodcutterRecipe> list = ((WoodcutterScreenHandler)this.handler).getAvailableRecipes();
+        for (int i = this.scrollOffset; i < scrollOffset && i < ((WoodcutterScreenHandler)this.handler).getAvailableRecipeCount(); ++i) {
+            int j = i - this.scrollOffset;
+            int k = x + j % 4 * 16;
+            int l = j / 4;
+            int m = y + l * 18 + 2;
+            this.client.getItemRenderer().renderInGuiWithOverrides(matrices, list.get(i).getOutput(this.client.world.getRegistryManager()), k, m);
+        }
+    }
+
+    @Override
+    public boolean mouseClicked(double mouseX, double mouseY, int button) {
+        this.mouseClicked = false;
+        if (this.canCraft) {
+            int i = this.x + 52;
+            int j = this.y + 14;
+            int k = this.scrollOffset + 12;
+            for (int l = this.scrollOffset; l < k; ++l) {
+                int m = l - this.scrollOffset;
+                double d = mouseX - (double)(i + m % 4 * 16);
+                double e = mouseY - (double)(j + m / 4 * 18);
+                if (!(d >= 0.0) || !(e >= 0.0) || !(d < 16.0) || !(e < 18.0) || !((WoodcutterScreenHandler)this.handler).onButtonClick(this.client.player, l)) continue;
+                MinecraftClient.getInstance().getSoundManager().play(PositionedSoundInstance.master(SoundEvents.UI_STONECUTTER_SELECT_RECIPE, 1.0f));
+                this.client.interactionManager.clickButton(((WoodcutterScreenHandler)this.handler).syncId, l);
+                return true;
+            }
+            i = this.x + 119;
+            j = this.y + 9;
+            if (mouseX >= (double)i && mouseX < (double)(i + 12) && mouseY >= (double)j && mouseY < (double)(j + 54)) {
+                this.mouseClicked = true;
+            }
+        }
+        return super.mouseClicked(mouseX, mouseY, button);
+    }
+
+    @Override
+    public boolean mouseDragged(double mouseX, double mouseY, int button, double deltaX, double deltaY) {
+        if (this.mouseClicked && this.shouldScroll()) {
+            int i = this.y + 14;
+            int j = i + 54;
+            this.scrollAmount = ((float)mouseY - (float)i - 7.5f) / ((float)(j - i) - 15.0f);
+            this.scrollAmount = MathHelper.clamp(this.scrollAmount, 0.0f, 1.0f);
+            this.scrollOffset = (int)((double)(this.scrollAmount * (float)this.getMaxScroll()) + 0.5) * 4;
+            return true;
+        }
+        return super.mouseDragged(mouseX, mouseY, button, deltaX, deltaY);
+    }
+
+    @Override
+    public boolean mouseScrolled(double mouseX, double mouseY, double amount) {
+        if (this.shouldScroll()) {
+            int i = this.getMaxScroll();
+            float f = (float)amount / (float)i;
+            this.scrollAmount = MathHelper.clamp(this.scrollAmount - f, 0.0f, 1.0f);
+            this.scrollOffset = (int)((double)(this.scrollAmount * (float)i) + 0.5) * 4;
+        }
+        return true;
+    }
+
+    private boolean shouldScroll() {
+        return this.canCraft && ((WoodcutterScreenHandler)this.handler).getAvailableRecipeCount() > 12;
+    }
+
+    protected int getMaxScroll() {
+        return (((WoodcutterScreenHandler)this.handler).getAvailableRecipeCount() + 4 - 1) / 4 - 3;
+    }
+
+    private void onInventoryChange() {
+        this.canCraft = ((WoodcutterScreenHandler)this.handler).canCraft();
+        if (!this.canCraft) {
+            this.scrollAmount = 0.0f;
+            this.scrollOffset = 0;
+        }
+    }
+}
+
+
